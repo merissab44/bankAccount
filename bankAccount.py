adeaab74d85f2d5627377de8967889b794cffae7
Merissa Bridgeman <merissa.bridgeman98@gmail.com>
2020-11-09 13:05:00 -0800

    fixed other interest

--- bankAccount.py
+++ bankAccount.py
@@ -10,7 +10,7 @@ class BankAccount:
         self.balance = balance
 # these are the start of the methods, first one depsits money to your account balance
     def deposit(self, amount):
-        self.balance += amount + self.balance
+        self.balance += amount
         print(f"Amount Deposited: ${amount}")
         return self.balance
 # this method subtracts money from account balance
@@ -29,12 +29,13 @@ class BankAccount:
         return balance
 # this method gives an overview of your account
     def print_receipt(self):
+        balance = self.balance
         print(self.name)
         # from w3schools this looks for digits in this string and replaces the first 4 with an asterisk
         account = re.sub("\d","*",str(self._BankAccount__account), 4)
         print(f"Account No.: {account}")
         print(f"Routing No.: {self._BankAccount__routing}")
-        print(f"Balance: ${self.balance}")
+        print(f"Balance: ${balance}")
 # initiated 3 new bank account objects
 merissa = BankAccount('Merissa Bridgeman', 19339494, 43940303939, 600)
 audrey = BankAccount('Audrey Byrne', 38492049, 498493947,400)
@@ -74,7 +75,7 @@ while True:
             amount = int(input("how much would you like to withdraw? "))
             print(audrey.withdraw(amount))
         elif choice == "3":
-            print(audrey.get_balance())
+            print(audrey.add_interest())
         elif choice == "4":
             print(audrey.print_receipt())
     elif person == "wendy":
@@ -90,6 +91,6 @@ while True:
             amount = int(input("how much would you like to withdraw? "))
             print(wendy.withdraw(amount))
         elif choice == "3":
-            print(wendy.get_balance())
+            print(wendy.add_interest())
         elif choice == "4":
             print(wendy.print_receipt())
